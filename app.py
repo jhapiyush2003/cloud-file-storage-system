42bf699ab54987ae609527895203a1845bcfa99c
Piyush Jha <PIYUSHJHA5377@GMAIL.COM>
2026-04-25 09:30:19 +0000

    Fixed app syntax

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="Cloud Storage")
 
 st.title("☁️ Piyush Cloud Storage")
-st.success("App under maintenance. Main dashboard restoring...")git pull origin main --rebase
+st.success("App under maintenance. Main dashboard restoring...")
